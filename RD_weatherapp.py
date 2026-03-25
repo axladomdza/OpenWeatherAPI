8d11e92cf66d7c550b106d2439fcedc646e71dd3
Axl Adomdza <elikojoaklade@gmail.com>
2026-03-25 06:32:33 +0000

    got to call from json file, now onto more functions

--- RD_weatherapp.py
+++ RD_weatherapp.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
@@ -7,7 +8,6 @@ API_KEY = os.getenv("API_KEY")
 
 import requests as r
 #Base URLs
-# CWEATHER_BASE = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
 
 
 def get_coords():
@@ -15,8 +15,8 @@ def get_coords():
         city_name = input("\nWhich city do you want to search for? (make sure to spell correctly)")
         limit = "50"    #make sure this is in quotes
         GEOCODE_BASE = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit={limit}&appid={API_KEY}"  #adds city name and limit, takes API key from "env"
-        response = r.get(GEOCODE_BASE) #gives a status code
-        coords_json = response.json()   #returns an empty list if nothing is found
+        coords_resp = r.get(GEOCODE_BASE) #gives a status code
+        coords_json = coords_resp.json()   #returns an empty list if nothing is found
         
         if len(coords_json) == 0:   #location check
             print("City not found, check your spelling and try again.")
@@ -24,6 +24,21 @@ def get_coords():
         
         lat = coords_json[0]['lat'] #finds latitude
         lon = coords_json[0]['lon'] #finds longitude
-        return lat, lon
+        return lat, lon, city_name
 
 #NEXT: Put coords into weather url to truly start
+
+def get_weather(lat, lon, city_name):
+    CWEATHER_BASE = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric"
+    weath_resp = r.get(CWEATHER_BASE)   #getting the data from the URL
+
+    data = weath_resp.json() #reading the file stream directly
+
+    weath_descr = data['weather'][0]['description'] #finding the weather description
+    weath_maintemp = data['main']['temp']   #finding the main temperature
+    print(f"The weather in {city_name} is {weath_descr} and the temperature is {weath_maintemp} Celsius")   #putting vars into a string
+    # user_choice = input("What action would you like to do?\n 1 - ")
+
+
+lat, lon, city_name = get_coords()
+get_weather(lat, lon, city_name)
