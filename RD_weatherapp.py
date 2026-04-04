946011c89bf6cfb6978bfc3ed024f53dce623b2d
Axl Adomdza <elikojoaklade@gmail.com>
2026-04-04 07:42:23 +0000

    Added comments and docstrings

--- RD_weatherapp.py
+++ RD_weatherapp.py
@@ -1,4 +1,3 @@
-#Hello! This is where all the building gets done, and when it's finished I'll polish it up and remove the comments.
 from dotenv import load_dotenv
 import os
 import json
@@ -10,34 +9,39 @@ import requests as r
 
 
 def get_coords():
+    '''
+    Prompts ths user for a city name and fetches the coordinates using the OpenWeatherMap and Geocoding API
+    Returns:
+        tuple: (lat, lon, city_name)
+    '''
     while True:
         city_name = input("\nWhich city do you want to search for? (make sure to spell correctly)")
-        limit = "50"    #make sure this is in quotes
-        GEOCODE_BASE = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit={limit}&appid={API_KEY}"  #adds city name and limit, takes API key from "env"
-        coords_resp = r.get(GEOCODE_BASE) #gives a status code
-        coords_json = coords_resp.json()   #returns an empty list if nothing is found
+        limit = "50"
+        GEOCODE_BASE = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit={limit}&appid={API_KEY}"
+        coords_resp = r.get(GEOCODE_BASE)  # Returns 200 even if city is not found
+        coords_json = coords_resp.json()
         
-        if len(coords_json) == 0:   #location check
+        if len(coords_json) == 0:
             print("City not found, check your spelling and try again.")
             continue
         
-        lat = coords_json[0]['lat'] #finds latitude
-        lon = coords_json[0]['lon'] #finds longitude
+        lat = coords_json[0]['lat']
+        lon = coords_json[0]['lon'] 
         return lat, lon, city_name
 
-#NEXT: Put coords into weather url to truly start
 
 def get_weather(lat, lon, name):
-    CWEATHER_BASE = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=imperial"
-    weath_resp = r.get(CWEATHER_BASE)   #getting the data from the URL
+    '''
+    Uses coords from get_coords to get weather details 
+    '''
+    CWEATHER_BASE = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=imperial" #Adding the longitude and latitude to the URL request
+    weath_resp = r.get(CWEATHER_BASE) 
+    weath_data = weath_resp.json()
+    weath_descr = weath_data['weather'][0]['description']  # Finding the weather description
+    weath_maintemp = weath_data['main']['temp']  # Finding the main temperature
+    print(f"The weather in {name} is {weath_descr} and the temperature is {weath_maintemp} Fahreinheit.")  # Prints city and weather conditions together
+    
 
-    data = weath_resp.json() #reading the file stream directly
 
-    weath_descr = data['weather'][0]['description'] #finding the weather description
-    weath_maintemp = data['main']['temp']   #finding the main temperature
-    print(f"The weather in {name} is {weath_descr} and the temperature is {weath_maintemp} Fahreinheit")   #putting vars into a string
-    # user_choice = input("What action would you like to do?\n 1 - ")
-
-
-lat, lon, city_name = get_coords() #introducing arguments
-get_weather(lat, lon, city_name) #running with those arguments
+lat, lon, city_name = get_coords() 
+get_weather(lat, lon, city_name)
