88e7a29f911152d7aff6ebc926ab2edfa8c36ae7
Axl Adomdza <elikojoaklade@gmail.com>
2026-04-11 13:29:16 +0000

    removed .env file form github

--- RD_weatherapp.py
+++ RD_weatherapp.py
@@ -15,7 +15,7 @@ def get_coords():
         tuple: (lat, lon, city_name)
     '''
     while True:
-        city_name = input("\nWhich city do you want to search for? (make sure to spell correctly)")
+        city_name = input("\nWhich city do you want to search for? (make sure to spell correctly): ")
         limit = "50"
         GEOCODE_BASE = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit={limit}&appid={API_KEY}"
         coords_resp = r.get(GEOCODE_BASE)  # Returns 200 even if city is not found
@@ -43,14 +43,16 @@ def get_weather(lat, lon, name):
     weath_resp = r.get(CWEATHER_BASE) 
     weath_data = weath_resp.json()
     weath_descr = weath_data['weather'][0]['description']  # Finding the weather description
-    weath_maintemp = weath_data['main']['temp']  # Finding the main temperature
+    weath_maintemp = weath_data['main']['temp']             # Finding the main temperature
     print(f"The weather in {name} is {weath_descr} and the temperature is {weath_maintemp} Fahreinheit.")  # Prints city and weather conditions together
-    
+
 def temp_details(lat, lon, name):
     TEMP_BASE = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=imperial"
     tempweath_resp = r.get(TEMP_BASE)
     temp_data = tempweath_resp.json()
-
+    temp_high = temp_data['main']['temp_max']   # Getting maximum temperature
+    temp_low = temp_data['main']['temp_min']    # Getting minimum temperature
+    print(f"In {name} the maximum temperature is {temp_high} and the minimum temperature is {temp_low}")
 
 lat, lon, city_name = get_coords() 
-get_weather(lat, lon, city_name)
+temp_details(lat, lon, city_name)
